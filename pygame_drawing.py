440290ba6053d6e5f66b0e15834ac9113f839fef
David Gilpin <dgilpin@bmchs.org>
2021-11-05 08:28:15 -0500

    finished drawing

--- pygame_drawing.py
+++ pygame_drawing.py
@@ -25,11 +25,6 @@ DISPLAYSURF = pygame.display.set_mode((WIDTH, HEIGHT))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("my drawing")
 
-# create lines and shapes
-pygame.draw.line(DISPLAYSURF, BLUE, (150, 130), (130, 170))
-pygame.draw.line(DISPLAYSURF, BLUE, (150, 130), (170, 170))
-pygame.draw.line(DISPLAYSURF, GREEN, (130, 170), (170, 170))
-pygame.draw.circle(DISPLAYSURF, BLACK, (100, 50), 30)
 
 
 
@@ -40,6 +35,15 @@ while True:
             pygame.quit()
             sys.exit()
 
+    # create lines and shapes
+    pygame.draw.line(DISPLAYSURF, BLUE, (150, 130), (130, 170))
+    pygame.draw.line(DISPLAYSURF, BLUE, (150, 130), (170, 170))
+    pygame.draw.line(DISPLAYSURF, GREEN, (130, 170), (170, 170))
+    pygame.draw.circle(DISPLAYSURF, BLACK, (100, 50), 30)
+    pygame.draw.circle(DISPLAYSURF, BLACK, (200, 50), 30)
+    pygame.draw.rect(DISPLAYSURF, RED, (100, 200, 100, 50), 2)
+    pygame.draw.rect(DISPLAYSURF, BLACK, (110, 260, 80, 5))
+    
     # refresh screen
     pygame.display.update()
     clock.tick(FPS)
